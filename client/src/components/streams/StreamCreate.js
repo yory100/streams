19a10189b974e441ff1f869ad7f7a2670c05e48b
yory <yory_man@yahoo.com>
2019-09-08 13:19:25 +0300

    init post method

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
     renderError = ({ error, touched }) => {
@@ -22,7 +24,9 @@ class StreamCreate extends React.Component {
         );
     };
 
-    onSubmit({ title, description }) {}
+    onSubmit = formValues => {
+        this.props.createStream(formValues);
+    };
 
     render() {
         return (
@@ -59,7 +63,12 @@ const validate = ({ title, description }) => {
     return errors;
 };
 
-export default reduxForm({
+const formWrapper = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(
+    null,
+    { createStream }
+)(formWrapper);
